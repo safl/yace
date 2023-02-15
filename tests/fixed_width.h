0dd6dd09eaab0b1d0fdc40c28ea511a33a6dad64
Simon A. F. Lund <os@safl.dk>
2023-02-15 20:17:04 +0100

    tests: fixed_width
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ tests/fixed_width.h
@@ -0,0 +1,13 @@
+struct integer_signed_fixed {	
+	int8_t field_i8;
+	int16_t field_i16;
+	int32_t field_i32;
+	int64_t field_i64;
+};
+
+struct integer_unsigned_fixed {	
+	uint8_t field_u8;
+	uint16_t field_u16;
+	uint32_t field_u32;
+	uint64_t field_u64;
+};
